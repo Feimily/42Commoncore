7ec31357b4a387546750ed5e86634c972393f0d8
Linded_ <91735533+Feimily@users.noreply.github.com>
2025-10-13 17:58:48 +0200

    Strtrim medio arreglado

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vaguayo- <vaguayo-@student.42barcelon      +#+  +:+       +#+        */
+/*   By: vaguayo- <vaguayo-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 12:53:05 by vaguayo-          #+#    #+#             */
-/*   Updated: 2025/10/05 09:57:37 by vaguayo-         ###   ########.fr       */
+/*   Updated: 2025/10/13 16:57:45 by vaguayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+
 #include <string.h>
 
 size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (s && s[i])
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,29 +1,5 @@
-static int  ft_count_set(const char *s1, const char *set)
+static int to_set(const char c, const char *set)
 {
-    size_t i;
-    size_t j;
-    int res;
-
-    i = 0;
-    j = 0;
-    res = 0;
-    while (s1[i])
-    {
-        while (set[j])
-        {
-            if (s1[i] == set[j])
-                res++;
-            j++;
-        }
-        j = 0
-        i++;
-    }
-    return (res)
-}
-
-static int to_trim(const char c, const char *set)
-{
-    size_t i;
     size_t j;
 
     j = 0;
@@ -31,33 +7,68 @@ static int to_trim(const char c, const char *set)
         {
             if (c == set[j])
                 return (1);
-            j++ 
+            j++;
         }
     
     return (0)
 }
+
+static int  ft_count_set_front(const char *s1, const char *set)
+{
+    size_t i;
+    int res;
+
+    i = 0;
+    res = 0;
+    while (s1[i] && to_set(s1[i], set))
+    {
+        res++;
+        i++;
+    }
+    return (res)
+}
+
+static int  ft_count_set_back(const char *s1, const char *set, size_t len)
+{
+    size_t i;
+    int res;
+
+    i = len - 1;
+    res = 0;
+    if (len == 0)
+        return (0);
+    while (s1[i] && to_set(s1[i], set))
+    {
+        res++;
+        i--;
+    }
+    return (res)
+}
+
 char    *ft_strtrim(const char *s1, const char *set)
 {
-    size_t  i
+    size_t  i;
     size_t  j;
+    size_t  len;
+    size_t  setlen;
     char    *str;
     
-    len = ft_strlen(s1) - ft_count_set(s1, set);
-
-    str = malloc(len * size_of(char) + 1)
-    if (!str)
-        return (NULL);
+    setlen = ft_count_set_front(s1, set) + ft_count_set_back(s1, set, ft_strlen(s1))
+    if (setlen >= ft_strlen(s1))
+        return NULL
+    len = (ft_strlen(s1) - len);
+    ft_calloc(len, sizeof(char))
     i = 0;
     j = 0;
     while (str[i])
     {
-        if (to_trim(s1[j], set))
+        if (to_set(s1[j], set))
             j++;
         else
         {
             str[i] = s1[j] 
-            i++
-            j++
+            i++;
+            j++;
         }
     }
     return (str)
